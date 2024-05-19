418940eb02a4e8772720df65ad450d61560a5c75
AnnaMolly93 <78878680+AnnaMolly93@users.noreply.github.com>
2024-05-19 18:11:44 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const noBtn = document.getElementById('no-btn');
-
-noBtn.addEventListener('mouseover', () => {
-    const x = Math.random() * (window.innerWidth - noBtn.clientWidth);
-    const y = Math.random() * (window.innerHeight - noBtn.clientHeight);
-    noBtn.style.left = `${x}px`;
-    noBtn.style.top = `${y}px`;
-});
